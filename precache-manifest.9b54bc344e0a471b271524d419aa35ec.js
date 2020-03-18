0365fc86f7f769de4bd1baf10e03f8b0011963d2
unknown <hecrabbs@gmail.com>
2020-03-18 10:29:35 -0700

    Updates

---
+++ precache-manifest.9b54bc344e0a471b271524d419aa35ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168f7dc4501a5b419ca8009c3ae22ea1",
+    "url": "/mozillaApp/index.html"
+  },
+  {
+    "revision": "e71dbc2fe43f49218dd8",
+    "url": "/mozillaApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0380f7a3878f5b660ae5",
+    "url": "/mozillaApp/static/js/2.87742fe5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mozillaApp/static/js/2.87742fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71dbc2fe43f49218dd8",
+    "url": "/mozillaApp/static/js/main.63d93955.chunk.js"
+  },
+  {
+    "revision": "765df5c12cd2a940bdda",
+    "url": "/mozillaApp/static/js/runtime-main.c43035a1.js"
+  }
+]);
