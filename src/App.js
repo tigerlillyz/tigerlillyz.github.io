c7eea1ffc5757f69bb4f5d67e4a55dd2773e9189
tigerlillyz <tigerlillyzietz@gmail.com>
2020-03-19 12:17:33 -1000

    added template not working

--- src/App.js
+++ src/App.js
@@ -1,61 +1,72 @@
 import React, { useState } from "react";
 import './App.css';
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+//import lines from './images/lines.png';
+
+import home from './pages/home';
+
 
 function App() {
 
-  const [candidateId, setCandidateId] = useState("");
-  const [candidates, setCandidates] = useState([]);
+  // const [candidateId, setCandidateId] = useState("");
+  // const [candidates, setCandidates] = useState([]);
 
-  function GetCandidateId(props) {
-    const url = `https://api.open.fec.gov/v1/names/candidates/?q=${props}&api_key=SVuK6wlixoKEc7Ccdd7X2paVLHTAjGjJUZdlzAMp`
-    fetch(url)
-      .then(r => r.json())
-      .then(r => r.results)
-      .then(r => {
-        setCandidateId(r[0].id);
-        GetCandidateFromId(r[0].id);
-      });
-  }
+  // function GetCandidateId(props) {
+  //   const url = `https://api.open.fec.gov/v1/names/candidates/?q=${props}&api_key=SVuK6wlixoKEc7Ccdd7X2paVLHTAjGjJUZdlzAMp`
+  //   fetch(url)
+  //     .then(r => r.json())
+  //     .then(r => r.results)
+  //     .then(r => {
+  //       setCandidateId(r[0].id);
+  //       GetCandidateFromId(r [0].id);
+  //     });
+  // }
 
-  function GetCandidateFromId(props) {
-    const url = `https://api.open.fec.gov/v1/candidate/${props}/?sort=name&page=1&api_key=SVuK6wlixoKEc7Ccdd7X2paVLHTAjGjJUZdlzAMp&sort_null_only=false&sort_nulls_last=false&sort_hide_null=false&per_page=20`
-    fetch(url)
-      .then(r => r.json())
-      .then(r => r.results)
-      .then(r => setCandidates(r[0]));
-  }
+  // function GetCandidateFromId(props) {
+  //   const url = `https://api.open.fec.gov/v1/candidate/${props}/?sort=name&page=1&api_key=SVuK6wlixoKEc7Ccdd7X2paVLHTAjGjJUZdlzAMp&sort_null_only=false&sort_nulls_last=false&sort_hide_null=false&per_page=20`
+  //   fetch(url)
+  //     .then(r => r.json())
+  //     .then(r => r.results)
+  //     .then(r => setCandidates(r[0]));
+  // }
 
-  function SearchBar() {
-    const [input, setInput] = useState({});
+  // function SearchBar() {
+  //   const [input, setInput] = useState({});
 
-    const handleInputChange = (e) => setInput({
-      ...input,
-      [e.currentTarget.name]: e.currentTarget.value
-    })
+  //   const handleInputChange = (e) => setInput({
+  //     ...input,
+  //     [e.currentTarget.name]: e.currentTarget.value
+  //   })
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      GetCandidateId(e.currentTarget.candidateName.value);
-    }
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     GetCandidateId(e.currentTarget.candidateName.value);
+  //   }
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <label>Candidate Name:</label>
-        <input type="text" name="candidateName" onChange={handleInputChange} />
-        <input type="submit" />
-      </form>
-    )
-  }
+    // return (
+    //   <form onSubmit={handleSubmit}>
+    //     <label>Candidate Name:</label>
+    //     <input type="text" name="candidateName" onChange={handleInputChange} />
+    //     <input type="submit" />
+    //   </form>
+    // )
+  //}
 
   return (
-    <div className="container">
-      <SearchBar />
+    <div className="wrapper">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+        </Switch>
+      </Router>
+      {/* <SearchBar />
       <h2>Candidates and Party</h2>
       {candidateId} <br />
        <div>
          Name: {candidates.name} <br />
          Party: {candidates.party_full}
-       </div>
+       </div> */}
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+class Home extends Component {
+    render () {
+        return (
+            <div>
+
+            
+            <Header />
+            //here is the location for us to insert html files or code to render to rest of the oage
+            <Footer /> 
+            </div>
+
+        )
+    }
+} export default Home;  
