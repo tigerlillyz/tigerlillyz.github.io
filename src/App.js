3adb861770d05a95cd5e82c8fbdac7818b7054f0
unknown <hecrabbs@gmail.com>
2020-03-19 23:12:02 -0700

    gets most recent id

--- src/App.js
+++ src/App.js
@@ -2,27 +2,65 @@ import React, { useState } from "react";
 
 function App() {
 
-  const [candidateId, setCandidateId] = useState("");
-  const [candidates, setCandidates] = useState([]);
+  const [candidateInfo, setCandidateInfo] = useState([]);
 
-  function GetCandidateId(props) {
+  function FetchArrayOfIds(props) {
     const url = `https://api.open.fec.gov/v1/names/candidates/?q=${props}&api_key=SVuK6wlixoKEc7Ccdd7X2paVLHTAjGjJUZdlzAMp`
     fetch(url)
+      .then(response => response.json())
+      .then((json) => {
+        handleJSON(json);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    const handleJSON = (json) => {
+      let results = json.results;
+      if (results.length === 0) {
+        throw Error("Invalid Entry");
+      } else {
+        let idArray = results.map(result => result.id);
+        FindMostRecentId(idArray);
+      }
+    }
+  }
+
+  function FindMostRecentId(props) {
+    const url = (props) => {
+      return `https://api.open.fec.gov/v1/candidate/${props}/?sort_nulls_last=false&sort_null_only=false&per_page=20&page=1&sort=name&api_key=SVuK6wlixoKEc7Ccdd7X2paVLHTAjGjJUZdlzAMp&sort_hide_null=false`
+    }
+    let urls = props.map(prop => url(prop));
+    Promise.all(urls.map(url => fetch(url)
       .then(r => r.json())
-      .then(r => r.results)
+    ))
+      .then((json) => {
+        handleJSON(json);
+      })
+
+    const handleJSON = (json) => {
+      let activeThroughDates = json.map(object => object.results[0].active_through);
+      let mostRecentDateIndex = activeThroughDates.indexOf(Math.max(...activeThroughDates));
+      let mostRecentId = props[mostRecentDateIndex];
+      GetCandidateFromId(mostRecentId);
+    }
+  }
+
+
+  function GetCandidateFromId(props) {
+    const url = `https://api.open.fec.gov/v1/candidate/${props}/?sort_nulls_last=false&sort_null_only=false&per_page=20&page=1&sort=name&api_key=SVuK6wlixoKEc7Ccdd7X2paVLHTAjGjJUZdlzAMp&sort_hide_null=false`
+    fetch(url)
+      .then(r => r.json())
       .then(r => {
-        setCandidateId(r[0].id);
-        GetCandidateFromId(r[0].id);
+        setCandidateInfo(r.results[0]);
       });
   }
 
-  function GetCandidateFromId(props) {
-    const url = `https://api.open.fec.gov/v1/candidate/${props}/?sort=name&page=1&api_key=SVuK6wlixoKEc7Ccdd7X2paVLHTAjGjJUZdlzAMp&sort_null_only=false&sort_nulls_last=false&sort_hide_null=false&per_page=20`
-    fetch(url)
-      .then(r => r.json())
-      .then(r => r.results)
-      .then(r => setCandidates(r[0]));
-  }
+  // function FetchHistory() { }
+  // function FetchCommittees() { }
+  // function FetchFinancial() { }
+  // function FetchLoans() { }
+  // function FetchDates() { }
 
   function SearchBar() {
     const [input, setInput] = useState({});
@@ -34,7 +72,7 @@ function App() {
 
     const handleSubmit = (e) => {
       e.preventDefault();
-      GetCandidateId(e.currentTarget.candidateName.value);
+      FetchArrayOfIds(e.currentTarget.candidateName.value);
     }
 
     return (
@@ -50,11 +88,16 @@ function App() {
     <div className="App">
       <SearchBar />
       <h2>Candidates and Party</h2>
-      {candidateId} <br />
-       <div>
-         Name: {candidates.name} <br />
-         Party: {candidates.party_full}
-       </div>
+      {candidateInfo.candidate_id} <br />
+      {candidateInfo.name} <br />
+      {candidateInfo.party_full} <br />
+      {candidateInfo.active_through} <br />
+      {candidateInfo.address_street_1} <br />
+      {candidateInfo.address_city} <br />
+      {candidateInfo.address_state} <br />
+      <div>
+
+      </div>
     </div>
   );
 }
